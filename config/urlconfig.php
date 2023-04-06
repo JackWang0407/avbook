1176023c7b562aec7057d85ab8e69c62781d27b1
JackWang0407 <jack@DESKTOP-1C3QDAN.localdomain>
2023-04-06 14:44:32 +0800

    update avmoo host

--- config/urlconfig.php
+++ config/urlconfig.php
@@ -1,6 +1,6 @@
 <?php
 
-return ['avmoohost'=>'avmoo.click', //https://avmoo.click/
+return ['avmoohost'=>'avmoo.cfd', //https://avmoo.click/
     'javbushost'=>'www.javbus.com', //https://www.javbus.com/
     'javlibhost'=>'www.javlibrary.com', //https://javlib.com/
     '7mmtvhost'=>'7mmtv.sx', //https://7mmtv.sx/
